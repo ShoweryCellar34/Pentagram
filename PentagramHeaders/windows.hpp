43c52a5f5a598bf34da51f43e574833ff2ea9673
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2024-05-15 14:59:02 +1200

    modified:   PentagramHeaders/windows.hpp

--- PentagramHeaders/windows.hpp
+++ PentagramHeaders/windows.hpp
@@ -7,6 +7,11 @@
 
 namespace PNT
 {
+
+    void keyboardCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+        std::find(Window::instanceList.begin(), Window::instanceList.end(), window)map
+    }
+
     struct windowData
     {
         std::string title = "";
@@ -21,7 +26,7 @@ namespace PNT
     {
     public:
         // Constructor/Destructor
-        Window(const char *title = "Title", unsigned short width = 600, unsigned short height = 600, SDL_WindowFlags flags = SDL_WINDOW_OPENGL)
+        Window(const char *title = "Title", unsigned short width = 600, unsigned short height = 600)
         {
             instances++;
             instanceList.push_back(this);
@@ -36,10 +41,8 @@ namespace PNT
 
             window = glfwCreateWindow(width, height, title, NULL, NULL);
             glfwMakeContextCurrent(window);
-            unsigned char currentDisplay = SDL_GetDisplayForWindow(window);
-            SDL_SetWindowPosition(window, (SDL_GetCurrentDisplayMode(currentDisplay)->w / 2) - (data.width / 2), (SDL_GetCurrentDisplayMode(currentDisplay)->h / 2) - (data.height / 2) + 1);
-
-            gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress);
+            glfwSetKeyCallback(window, keyboardCallback);
+            gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
             ImGuiContext = ImGui::CreateContext();
             ImGuiIO &io = ImGui::GetIO();
@@ -66,85 +69,38 @@ namespace PNT
         }
 
         // Starts the opengl and imgui frame for the window, returns the sdl error code (0 is success)..
-        int startFrame()
+        void startFrame()
         {
-            int errorCode = 0;
-            errorCode = SDL_GL_MakeCurrent(window, openglContext);
-            if(errorCode != 0)
-            {
-                log.log(2, SDL_GetError());
-                return errorCode;
-            }
+            glfwMakeContextCurrent(window);
+            int width, height;
+            glfwGetFramebufferSize(window, &width, &height);
+            glViewport(0, 0, width, height);
             glClearColor(data.clearColor[0], data.clearColor[1], data.clearColor[2], data.clearColor[3]);
             glClear(GL_COLOR_BUFFER_BIT);
             ImGui::SetCurrentContext(ImGuiContext);
             ImGui_ImplOpenGL3_NewFrame();
-            ImGui_ImplSDL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
             ImGui::NewFrame();
             if(startFrameListener != nullptr)
             {
                 startFrameListener(this);
             }
-            return errorCode;
         }
 
         // Hides the window, returns the sdl error code (0 is success). 
-        int endFrame()
+        void endFrame()
         {
-            int errorCode = 0;
             ImGui::Render();
             ImGuiIO &io = ImGui::GetIO();
-            glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
             ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-            errorCode = SDL_GL_SwapWindow(window);
+            glfwSwapBuffers(window);
+            GLFWwindow* backupContext = glfwGetCurrentContext();
             ImGui::UpdatePlatformWindows();
-            if(errorCode != 0)
-            {
-                log.log(2, SDL_GetError());
-            }
+            ImGui::RenderPlatformWindowsDefault();
+            glfwMakeContextCurrent(backupContext);
             if(endFrameListener != nullptr)
             {
                 endFrameListener(this);
-            }
-            return errorCode;
-        }
-
-        // Processes the current event, listener functionality supported (check setlistener() function for details).
-        void eventProcess()
-        {
-            if(event.window.windowID == windowID)
-            {
-                switch(event.window.type)
-                {
-                case SDL_EVENT_WINDOW_RESIZED:
-                    data.width = event.window.data1;
-                    data.height = event.window.data2;
-                    break;
-
-                case SDL_EVENT_WINDOW_SHOWN:
-                    data.visiblity = true;
-                    break;
-
-                case SDL_EVENT_WINDOW_HIDDEN:
-                    data.visiblity = false;
-                    break;
-
-                case SDL_EVENT_WINDOW_CLOSE_REQUESTED:
-                    setVisiblity(false);
-                    break;
-
-                case SDL_EVENT_WINDOW_MOVED:
-                    data.x = event.window.data1;
-                    data.y = event.window.data2;
-                    break;
-
-                default:
-                    break;
-                }
-                ImGui_ImplSDL3_ProcessEvent(&event);
-                ImGuiIO &io = ImGui::GetIO();
-                if(!io.WantCaptureKeyboard && keyboardEventListener != nullptr) keyboardEventListener(this);
-                if(!io.WantCaptureMouse && mouseEventListener != nullptr) mouseEventListener(this);
             }
         }
 
@@ -176,54 +132,38 @@ namespace PNT
 
         // Sets the data struct of the window.
         void setWindowData(windowData newData) {
-            if(!newData.title.empty()) setTitle(newData.title.c_str());
+            setTitle(newData.title.c_str());
             setDimentions(newData.width, newData.height);
             setPosition(newData.x, newData.y);
-            if(newData.vsyncMode != -1) setVsyncMode(newData.vsyncMode);
-            if(newData.visiblity != -1) setVisiblity(newData.visiblity);
+            setVsyncMode(newData.vsyncMode);
+            setVisiblity(newData.visiblity);
             setClearColor(newData.clearColor[0], newData.clearColor[1], newData.clearColor[2], newData.clearColor[3]);
         }
 
-        // Sets the title of the window, returns the sdl error code (0 is success).
-        int setTitle(const char *newTitle) {
-            int errorCode = 0;
-            errorCode = SDL_SetWindowTitle(window, newTitle);
-            if(errorCode != 0) log.log(2, SDL_GetError());
-            return errorCode;
+        // Sets the title of the window.
+        void setTitle(const char *title) {
+            glfwSetWindowTitle(window, title);
         }
 
-        // Sets the width and height of the window (-1 = unchanged), returns the sdl error code (0 is success).
-        int setDimentions(unsigned short newWidth, unsigned short newHeight) {
-            int errorCode = 0;
-            errorCode = SDL_SetWindowSize(window, newWidth == -1 ? data.width : newWidth, newHeight == -1 ? data.height : newHeight);
-            if(errorCode != 0) log.log(2, SDL_GetError());
-            return errorCode;
+        // Sets the width and height of the window.
+        void setDimentions(unsigned short width, unsigned short height) {
+            glfwSetWindowSize(window, width, height);
         }
 
-        // Sets the x and y coordinates of the window (-1 = unchanged), returns the sdl error code (0 is success).
-        int setPosition(int newX, int newY) {
-            int errorCode = 0;
-            errorCode = SDL_SetWindowPosition(window, newX == -1 ? data.x : newX, newY == -1 ? data.y : newY);
-            if(errorCode != 0) log.log(2, SDL_GetError());
-            return errorCode;
+        // Sets the x and y coordinates of the window.
+        void setPosition(unsigned int x, unsigned int y) {
+            glfwSetWindowPos(window, x, y);
         }
 
-        // Sets the visiblity of the window, returns the sdl error code (0 is success).
-        int setVisiblity(bool newVisiblity) {
-            int errorCode = 0;
-            data.visiblity = newVisiblity;
-            newVisiblity == true ? errorCode = SDL_RaiseWindow(window) : errorCode = SDL_HideWindow(window);
-            if(errorCode != 0) log.log(2, SDL_GetError());
-            return errorCode;
+        // Sets the visiblity of the window.
+        void setVisiblity(bool visiblity) {
+            visiblity == true ? glfwShowWindow(window) : glfwHideWindow(window);
         }
 
-        // Sets the vsync mode of the window (0 = off, 1 = on, -1 = adaptive), returns the sdl error code (0 is success).
-        int setVsyncMode(char newVsyncMode) {
-            int errorCode;
-            data.vsyncMode = newVsyncMode;
-            errorCode = SDL_GL_SetSwapInterval(newVsyncMode);
-            if(errorCode != 0) log.log(2, SDL_GetError());
-            return errorCode;
+        // Sets the vsync mode of the window (0 = off, 1 = on, -1 = adaptive).
+        void setVsyncMode(char vsyncMode) {
+            data.vsyncMode = vsyncMode;
+            glfwSwapInterval(vsyncMode);
         }
 
         // Sets the opengl clear color for the window (-1 = unchanged).
@@ -238,14 +178,7 @@ namespace PNT
         GLFWwindow *window;
 
         // Window data
-        unsigned char windowID;
         windowData data;
-
-        // Event data
-        static inline SDL_Event event = SDL_Event();
-        friend SDL_Event getEvent();
-        friend bool pollEvent();
-
 
         // ImGui data
         ImGuiContext *ImGuiContext;
@@ -261,5 +194,11 @@ namespace PNT
         static inline int instances;
         static inline std::vector<Window *> instanceList;
         char instanceID;
+
+        // Functions
+        friend void keyboardCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+        void keyboardEvent(GLFWwindow* window, int key, int scancode, int action, int mods) {
+
+        }
     };
 }
