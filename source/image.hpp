c9c5c1325e42afc87c016c038106efec9d5c2970
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2024-06-20 08:51:15 +1200

    modified:   source/image.hpp

--- source/image.hpp
+++ source/image.hpp
@@ -11,20 +11,33 @@ namespace PNT {
 
     public:
         // Returns true if image not null (null meaning invalid), and false if image is null.
-        bool valid() {return pixels == nullptr ? 0 : 1;}
+        bool valid() {return pixels == nullptr ? false : true;}
         // Loads an image from disk.
-        void load(const char* path, int channels) {pixels = stbi_load(path, &width, &height, &this->channels, channels);}
-        // Creats an ImGui draw call for the image (Requires loadOnGPU() to be called). 
+        void load(const char* path) {pixels = stbi_load(path, &width, &height, &this->channels, 4);}
+        // Creats an ImGui draw call for the image (Requires loadOnGPU()). 
         void ImGuiDraw(int width, int height) {ImGui::Image((void*)(intptr_t)textureID, ImVec2(width, height));}
+
+        // Set the filter modes for the GPU texture (Requires loadOnGPU()).
+        void minMagFilter(unsigned int min, unsigned int mag) {
+            glBindTexture(GL_TEXTURE_2D, textureID);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, min);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, mag);
+        }
+        // Set the wrapping mode for the GPU texture (Requires loadOnGPU()).
+        void wrappingMode(unsigned int S, unsigned int T) {
+            glBindTexture(GL_TEXTURE_2D, textureID);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, S);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, T);
+        }
         // Loads the image on to the GPU.
-        void loadOnGPU() {
+        void loadOnGPU(unsigned int min, unsigned int mag, unsigned int S, unsigned int T) {
             glGenTextures(1, &textureID);
             glBindTexture(GL_TEXTURE_2D, textureID);
 
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, min);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, mag);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, S);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, T);
 
             glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
         }
@@ -41,10 +54,10 @@ namespace PNT {
         unsigned char* getPixels() {return pixels;}
 
         image() {
-            width = 0, height = 0, channels = 0;
+            textureID = 0, width = 0, height = 0, channels = 0;
             pixels = nullptr;
         }
-        image(const char* path, short channels) {load(path, channels);}
+        image(const char* path) {load(path);}
         ~image() {
             unloadOffGPU();
             stbi_image_free(pixels);
